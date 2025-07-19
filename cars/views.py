ce9734bed94f5537310cff3f166232f98598b664
Paulo Lima <paullo.webdev@gmail.com>
2025-07-19 18:20:06 -0300

    criando nova rota new_car para cadastro dos carros pelo frontend, formulário, template e view

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from cars.models import Car
+from cars.forms import CarForm
 
 # Create your views here.
 def cars_view(request):
-    cars = Car.objects.all().order_by('-model')
+    cars = Car.objects.all().order_by('model')
     search = request.GET.get('search')
 
     if search:
@@ -14,3 +15,7 @@ def cars_view(request):
         'cars.html',
          {'cars': cars}
     )
+
+def new_car_view(request):
+    new_car_form = CarForm()
+    return render(request, 'new_car.html', { 'new_car_form' : new_car_form })
